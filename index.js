7bc0134fc2bd5083a6129e51b98c2e8f7182123f
Liz Wilson <lizriderwilson@gmail.com>
2021-06-07 13:35:22 -0400

    commit tree ca32ce6bbd297038a722c5393d182cd7beee3cc2

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(array, event) {
+    let messageArray = [];
+    for (let i = 0; i < array.length; i++) {
+        messageArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return messageArray;
+}
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
